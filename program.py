9cd3aefeaeaa3ee952d7be47e5690726847eb975
Студент <pro100tos8596@gmail.com>
2023-11-20 18:42:31 +0300

    addnewcommit

--- program.py
+++ program.py
@@ -1,8 +1,2 @@
-# I am author!
-def main():
-    print('My first git program')
-    print('And I change it every day')
-
-
-if __name__ == '__main__':
-    main()
+print("My first Git program!!!")
+# 2017 (c) Me
